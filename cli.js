674fb91acfa3d2a73ac900a58ef5da8203a3e489
Szymon Kaliski <hi@szymonkaliski.com>
2019-10-29 17:39:55 +0100

    small addon for downloading assets

--- cli.js
+++ cli.js
@@ -6,6 +6,7 @@ const yargs = require("yargs");
 const createCache = require("./utils/cache");
 const tasks = require("./tasks");
 const related = require("./related");
+const getAsset = require("./get-asset");
 
 const args = yargs
   .command("tasks", "find all tasks in given directory", {
@@ -13,7 +14,24 @@ const args = yargs
     overdue: { default: true },
     "files-with-matches": { default: false }
   })
-  .command("related [file]", "find all notes related to given file")
+  .command("related", "find all notes related to given file", yargs => {
+    yargs.option("file", {
+      demandOption: true,
+      describe: "input file to search for related ones"
+    });
+  })
+  .command(
+    "get-asset",
+    "downloads asset and returns markdown embed/link",
+    yargs => {
+      yargs
+        .option("url", { demandOption: true, describe: "url to asset" })
+        .option("file", {
+          demandOption: true,
+          describe: "file where the asset will be added"
+        });
+    }
+  )
   .command("clear-cache", "clear cache, will be rebuilt on next command")
   .option("dir", {
     alias: "d",
@@ -27,26 +45,39 @@ const dir = path.resolve(args.dir);
 
 const cache = createCache(dir);
 
-if (TYPE === "tasks") {
-  cache.parse();
+const COMMANDS = {
+  tasks: () => {
+    cache.parse();
 
-  tasks({
-    files: cache.getFiles(),
-    overdue: args.overdue,
-    days: args.days,
-    filesWithMatches: args["files-with-matches"]
-  });
+    tasks({
+      files: cache.getFiles(),
+      overdue: args.overdue,
+      days: args.days,
+      filesWithMatches: args["files-with-matches"]
+    });
 
-  cache.store();
-} else if (TYPE === "related") {
-  cache.parse();
+    cache.store();
+  },
 
-  related({
-    tfidf: cache.getTfidf(),
-    file: args.file.replace(`${dir}/`, "")
-  });
+  related: () => {
+    cache.parse();
 
-  cache.store();
-} else if (TYPE === "clear-cache") {
-  cache.clear();
-}
+    related({
+      tfidf: cache.getTfidf(),
+      file: args.file.replace(`${dir}/`, "")
+    });
+
+    cache.store();
+  },
+
+  "get-asset": () => {
+    getAsset({
+      file: args.file,
+      url: args.url
+    });
+  },
+
+  "clear-cache": cache.clear
+};
+
+COMMANDS[TYPE]();
---
+++ get-asset/index.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+const md5 = require("md5");
+const mkdirp = require("mkdirp");
+const path = require("path");
+const wget = require("node-wget");
+
+module.exports = options => {
+  const assetDir = path.join(path.dirname(options.file), ".assets");
+  const ext = path.extname(options.url);
+
+  mkdirp.sync(assetDir);
+
+  const tempName = path.join(assetDir, "download.temp");
+
+  wget({ url: options.url, dest: tempName }, (error, result, body) => {
+    if (error) {
+      console.log(error);
+      process.exit(1);
+    }
+
+    const hash = md5(body);
+    const finalName = `${hash}${ext}`;
+    fs.renameSync(tempName, path.join(assetDir, finalName));
+
+    console.log(`![](.assets/${finalName})
+[source](${options.url})
+    `);
+  });
+};
